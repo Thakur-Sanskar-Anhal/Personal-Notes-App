b94368a345745cfc03bea22367279d23a91aba67
Sanskar <thakursanskaranhal@gmail.com>
2022-09-09 21:21:23 +0530

    first commit

---
+++ app/src/main/java/com/personal_notes/MainActivity.java
@@ -0,0 +1,13 @@
+package com.personal_notes;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
