26655bea0ce901e51c22de4af96f5e0ffa7a86a0
Sanskar <thakursanskaranhal@gmail.com>
2022-09-09 21:32:01 +0530

    Added dependencies

---
+++ app/src/main/java/com/personal_notes/Note.java
@@ -0,0 +1,32 @@
+package com.personal_notes;
+
+import io.realm.RealmObject;
+
+public class Note extends RealmObject {
+    String title , description ;
+    long created_time;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getCreated_time() {
+        return created_time;
+    }
+
+    public void setCreated_time(long created_time) {
+        this.created_time = created_time;
+    }
+}
