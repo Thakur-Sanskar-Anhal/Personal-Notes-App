795447f60f442f5d91b03b46a126d9c49ebd0a13
Sanskar <thakursanskaranhal@gmail.com>
2022-09-10 16:54:43 +0530

    Added some new actvities and files

--- app/src/main/java/com/personal_notes/MainActivity.java
+++ app/src/main/java/com/personal_notes/MainActivity.java
@@ -1,7 +1,12 @@
 package com.personal_notes;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MaterialButton addNoteBtn = findViewById(R.id.newNotebtn);
+        addNoteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,AddNote.class));
+            }
+        });
     }
 }
