1751aad3a0093902053338255d92b7f5b9e871c2
Sanskar <thakursanskaranhal@gmail.com>
2022-09-11 21:15:11 +0530

    Added realm object to addnote.java

--- app/src/main/java/com/personal_notes/AddNote.java
+++ app/src/main/java/com/personal_notes/AddNote.java
@@ -3,6 +3,11 @@ package com.personal_notes;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import io.realm.Realm;
 
 public class AddNote extends AppCompatActivity {
 
@@ -10,5 +15,21 @@ public class AddNote extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_note);
+        EditText titleInput = findViewById(R.id.titleInput);
+        EditText descriptionInput = findViewById(R.id.description);
+        Button savebtn = findViewById(R.id.savebtn);
+
+        Realm.init(getApplicationContext());
+        Realm realm = Realm.getDefaultInstance()
+
+        savebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String title = titleInput.getText().toString();
+                String description = descriptionInput.getText().toString();
+                long createdTime = System.currentTimeMillis();
+
+            }
+        });
     }
 }
